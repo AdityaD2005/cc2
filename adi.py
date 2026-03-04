9ba012631d6d4be68ad85215321bcb4184d22917
AdityaD2005 <Kulkarniaditya2005@gmail.com>
2026-03-04 15:23:21 +0530

    Add files via upload

---
+++ adi.py
@@ -0,0 +1,12 @@
+a=5
+b=10
+
+result1 = a+b
+result2 = b-a
+result3 = a*b
+result4 = b/a
+
+print(f"the sum of {a} and {b} is {result1}")
+print(f"the sub of {a} and {b} is {result2}")
+print(f"the mul of {a} and {b} is {result3}")
+print(f"the div of {a} and {b} is {result4}")
